774fc50be8f2fdb8b043a00346ecbb42a0ad4959
Akshay0897 <akshay.aghera@thomsonreuters.com>
2019-11-01 17:34:52 +0530

    adding client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import BlogForm from './components/form.component';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BlogForm />
+    </div>
+  );
+}
+
+export default App;
